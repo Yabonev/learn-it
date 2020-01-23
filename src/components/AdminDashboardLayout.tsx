000430063dcb88c20af86a9e6fd2dda49b2c98f1
yabonev <yasen.bonev@zuhlke.com>
2020-01-23 23:29:07 +0200

    Implemented basic loading and error handling in Admin dashboard

--- src/components/AdminDashboardLayout.tsx
+++ src/components/AdminDashboardLayout.tsx
@@ -1,11 +1,16 @@
 import React from "react";
+import Loader from "react-loader-spinner";
 import TopicsList from "./TopicsList";
 import { useTopics } from "../hooks/topics";
 
 const AdminDashboardLayout: React.FC = () => {
-  const { topics } = useTopics();
+  const { topics, error, loading } = useTopics();
 
-  return <TopicsList topics={topics} />;
+  const loader = loading && <Loader />;
+  const errorMessage = error && <p>Error occurred : {error}</p>;
+  const topicsList = <TopicsList topics={topics} />;
+
+  return <>{loader || errorMessage || topicsList}</>;
 };
 
 export default AdminDashboardLayout;
