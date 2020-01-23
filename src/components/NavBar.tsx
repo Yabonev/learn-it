5de7010b343c3952174d2446f105e6b53e0e0792
yabonev <yasen.bonev@zuhlke.com>
2020-01-23 22:44:36 +0200

    Refactored navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,32 +9,32 @@ import {
 } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
-function NavBar(props) {
+const NavBar: React.FC = _ => {
   return (
     <AppBar color="primary" position="static">
       <Toolbar>
         <List component="nav">
           <ListItem component="div">
             <ListItemText inset>
-              <Typography color="inherit" variant="h1">
+              <Typography color="inherit" variant="h3">
                 <Link to="/">Home</Link>
               </Typography>
             </ListItemText>
 
             <ListItemText inset>
-              <Typography color="inherit" variant="h1">
+              <Typography color="inherit" variant="h3">
                 <Link to="/admin-dashboard">Admin Panel</Link>
               </Typography>
             </ListItemText>
 
             <ListItemText inset>
-              <Typography color="inherit" variant="h1">
+              <Typography color="inherit" variant="h3">
                 <Link to="/about"> About</Link>
               </Typography>
             </ListItemText>
 
             <ListItemText inset>
-              <Typography color="inherit" variant="h1">
+              <Typography color="inherit" variant="h3">
                 <Link to="/details"> Course Details(static)</Link>
               </Typography>
             </ListItemText>
@@ -43,6 +43,6 @@ function NavBar(props) {
       </Toolbar>
     </AppBar>
   );
-}
+};
 
 export default NavBar;
