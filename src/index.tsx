997f034ef4572b767fbbe03ff614557d2c8fef62
yabonev <yasen.bonev@zuhlke.com>
2020-01-23 22:57:23 +0200

    Removed unnecessary sdk

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import * as firebase from "firebase";
-import firebaseConfig from "./firebase-config";
 
-firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
