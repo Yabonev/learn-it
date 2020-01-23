9787bc13bd78a2a8e9ad28726e5d3e833948f602
yabonev <yasen.bonev@zuhlke.com>
2020-01-23 22:58:02 +0200

    Refactored home page to typescript FC

---
+++ src/components/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Home: React.FC = () => (
+  <h1>
+    This is the home page. The starting point of your application. Move to{" "}
+    <i>Admin Dashboard</i> for more functionality.
+  </h1>
+);
+
+export default Home;
