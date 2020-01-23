45b42f99b5528beef7b732716e075a5e60f613a9
yabonev <yasen.bonev@zuhlke.com>
2020-01-23 20:02:08 +0200

    Added dummy course data

---
+++ src/data/courses.js
@@ -0,0 +1,12 @@
+const courses = [
+  { id: 1, topic: "Variables, types and the console", subtopicsCount: 4 },
+  { id: 2, topic: "Flow control", subtopicsCount: 4 },
+  { id: 3, topic: "Loops", subtopicsCount: 3 },
+  { id: 4, topic: "Functions", subtopicsCount: 6 },
+  { id: 5, topic: "Arrays", subtopicsCount: 2 },
+  { id: 6, topic: "Algorithms on arrays", subtopicsCount: 12 },
+  { id: 7, topic: "Strings", subtopicsCount: 8 },
+  { id: 8, topic: "Recursion", subtopicsCount: 5 }
+];
+
+export default courses;
