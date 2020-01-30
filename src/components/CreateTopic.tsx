e7238b85c6d184acf7c69939ddf68bede410c70f
yabonev <yasen.bonev@zuhlke.com>
2020-01-30 09:29:16 +0200

    Added dummy file for create topic form

--- src/components/CreateTopic.tsx
+++ src/components/CreateTopic.tsx
@@ -1,8 +1,19 @@
 import * as React from "react";
-
+import { useForm } from "react-hook-form";
+import { Input, TextField } from "@material-ui/core";
 const CreateTopic: React.FC = () => {
+  const { handleSubmit, register, reset, control } = useForm();
+
+  const onFormSubmit = (values: unknown) => {
+    console.log(values);
+  };
   return (
-    <h4>TODO: Implement form with useForm's Controller and Material-ui</h4>
+    <>
+      <h3>Create Topic</h3>
+      <form onSubmit={handleSubmit(onFormSubmit)}>
+        <TextField label="Topic name" type="text" />
+      </form>
+    </>
   );
 };
 
